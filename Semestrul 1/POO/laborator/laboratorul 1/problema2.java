e6264384d53098a65a5064b2f5c4ebd42f12d7fb
Alexandruyt4 <alexandruhaidet@gmail.com>
2024-10-09 20:37:56 +0300

    POO

---
+++ Semestrul 1/POO/laborator/laboratorul 1/problema2.java
@@ -0,0 +1,16 @@
+
+/*
+ Scrieti un program Java care initializeaza doua variablile ıntregi cu doua valori
+constante oarecare. In continuare, programul va determina variabila ce contine
+valoarea maxima ¸si va tipari continutul ei pe ecran
+ */
+class problema2{    
+    public static void main(String argv[])
+    {
+        int a=34,b=22;
+        if(a>b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+    }
+}
---
+++ Semestrul 1/POO/laborator/laboratorul 1/problema3.java
@@ -0,0 +1,12 @@
+class problema3{    
+    public static void main(String argv[])
+    {
+        int i,s=0;
+        for(i=1;i<=100;i+=2)
+        {
+            s+=i+1;
+            System.out.println(i);
+        }
+        System.out.println(s);
+    }
+}
