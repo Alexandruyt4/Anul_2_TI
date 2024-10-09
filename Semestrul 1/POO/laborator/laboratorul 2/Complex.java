1df1c6945dbd844fe094215b6fa05e4106a4452f
Alexandruyt4 <alexandruhaidet@gmail.com>
2024-10-09 23:39:24 +0300

    Create Complex.java

---
+++ Semestrul 1/POO/laborator/laboratorul 2/Complex.java
@@ -0,0 +1,22 @@
+class Complex {
+    double real;
+    double imaginar;
+
+    public Complex(double r,double i)
+    {
+        real = r;
+        imaginar = i;
+    }
+
+    private void afisare(double r,double i)
+    {
+        System.out.println(r + "+i*" + i);
+    } 
+
+
+    public static void main(String argv[])
+    {
+        Complex(2,3);
+    }
+}
+
